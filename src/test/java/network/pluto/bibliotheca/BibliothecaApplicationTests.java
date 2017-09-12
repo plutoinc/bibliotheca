2d49b6f74016a3dc53547dd66a925d321b8ae7af
tedyeee <tedyeee@gmail.com>
2017-09-12 17:36:03 +0900

    Remove test

--- src/test/java/network/pluto/bibliotheca/BibliothecaApplicationTests.java
+++ src/test/java/network/pluto/bibliotheca/BibliothecaApplicationTests.java
@@ -1,12 +1,9 @@
 package network.pluto.bibliotheca;
 
-import network.pluto.bibliotheca.configurations.BibliothecaConfiguration;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -16,12 +13,6 @@ public class BibliothecaApplicationTests {
 
     @Test
     public void contextLoads() {
-    }
-
-    @SpringBootApplication
-    @Import(BibliothecaConfiguration.class)
-    static class TestConfiguration {
-
     }
 
 }

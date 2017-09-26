67d30a8f1c40cb5929247c40b57e71cb55e7b21a
am2rican5 <am2rican5@gmail.com>
2017-09-26 16:13:06 +0900

    make createdAt readable

--- src/main/java/network/pluto/bibliotheca/models/BaseEntity.java
+++ src/main/java/network/pluto/bibliotheca/models/BaseEntity.java
@@ -1,5 +1,6 @@
 package network.pluto.bibliotheca.models;
 
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
+@Getter
 public abstract class BaseEntity implements Serializable {
 
     @CreatedDate

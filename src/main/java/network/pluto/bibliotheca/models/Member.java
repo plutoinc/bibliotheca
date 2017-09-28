7be7c5d2a99eb302a359e302c3491dca14e2676e
am2rican5 <am2rican5@gmail.com>
2017-09-28 14:25:02 +0900

    organization field was appended on member

--- src/main/java/network/pluto/bibliotheca/models/Member.java
+++ src/main/java/network/pluto/bibliotheca/models/Member.java
@@ -36,6 +36,9 @@ public class Member extends BaseEntity {
     @Column
     private String profileImage;
 
+    @Column
+    private String organization;
+
     @OneToOne
     @JoinColumn(name = "WALLER_ID")
     private Wallet wallet;

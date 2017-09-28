7b8ba05a5861528bff7d0211347bc8ac2e223bb1
am2rican5 <am2rican5@gmail.com>
2017-09-28 14:34:37 +0900

    findbyarticle on evaluation

--- src/main/java/network/pluto/bibliotheca/repositories/EvaluationRepository.java
+++ src/main/java/network/pluto/bibliotheca/repositories/EvaluationRepository.java
@@ -1,7 +1,11 @@
 package network.pluto.bibliotheca.repositories;
 
+import network.pluto.bibliotheca.models.Article;
 import network.pluto.bibliotheca.models.Evaluation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EvaluationRepository extends JpaRepository<Evaluation, Long> {
+    List<Evaluation> findByArticle(Article article);
 }

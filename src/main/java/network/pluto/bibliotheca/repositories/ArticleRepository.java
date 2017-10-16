7f8b081f951ef941e6116375c5490d3669683be2
tedyeee <tedyeee@gmail.com>
2017-10-16 17:06:49 +0900

    Add article / evaluation by createdBy query

--- src/main/java/network/pluto/bibliotheca/repositories/ArticleRepository.java
+++ src/main/java/network/pluto/bibliotheca/repositories/ArticleRepository.java
@@ -1,7 +1,11 @@
 package network.pluto.bibliotheca.repositories;
 
 import network.pluto.bibliotheca.models.Article;
+import network.pluto.bibliotheca.models.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+    List<Article> findByCreatedBy(Member createdBy);
 }
--- src/main/java/network/pluto/bibliotheca/repositories/EvaluationRepository.java
+++ src/main/java/network/pluto/bibliotheca/repositories/EvaluationRepository.java
@@ -5,8 +5,12 @@ import network.pluto.bibliotheca.models.Evaluation;
 import network.pluto.bibliotheca.models.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EvaluationRepository extends JpaRepository<Evaluation, Long> {
     long countByArticle(Article article);
 
     boolean existsByArticleAndCreatedBy(Article article, Member createdBy);
+
+    List<Evaluation> findByCreatedBy(Member createdBy);
 }

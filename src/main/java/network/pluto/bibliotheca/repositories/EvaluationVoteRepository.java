315786af6b156b727e2934c2eadf5e7bb4091f21
tedyeee <tedyeee@gmail.com>
2017-10-16 21:01:24 +0900

    Modify voted evaluation get query

--- src/main/java/network/pluto/bibliotheca/repositories/EvaluationVoteRepository.java
+++ src/main/java/network/pluto/bibliotheca/repositories/EvaluationVoteRepository.java
@@ -12,6 +12,6 @@ import java.util.List;
 public interface EvaluationVoteRepository extends JpaRepository<EvaluationVote, Long> {
     boolean existsByMemberAndEvaluation(Member member, Evaluation evaluation);
 
-    @Query("select e.evaluation.evaluationId from EvaluationVote e where e.member.memberId = :memberId and e.evaluation.evaluationId in :evaluationIds")
-    List<Long> getVotedEvaluationIds(@Param("memberId") long memberId, @Param("evaluationIds") List<Long> evaluationIds);
+    @Query("select e.evaluation.evaluationId from EvaluationVote e where e.member = :member and e.evaluation in :evaluations")
+    List<Long> getVotedEvaluationIds(@Param("member") Member member, @Param("evaluations") List<Evaluation> evaluations);
 }

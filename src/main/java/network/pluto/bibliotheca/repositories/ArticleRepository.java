e6006d6702cf23d6ec4dd9198a9bf50a13d978c4
tedyeee <tedyeee@gmail.com>
2017-10-20 16:09:46 +0900

    add article batch query

--- src/main/java/network/pluto/bibliotheca/repositories/ArticleRepository.java
+++ src/main/java/network/pluto/bibliotheca/repositories/ArticleRepository.java
@@ -6,6 +6,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ArticleRepository extends JpaRepository<Article, Long> {
     Page<Article> findByCreatedBy(Member createdBy, Pageable pageable);
+
+    Page<Article> findByArticleIdIn(List<Long> articleIds, Pageable pageable);
 }

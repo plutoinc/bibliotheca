efb7d554936dcf3b38077dc02191722d0d2007da
tedyeee <tedyeee@gmail.com>
2017-11-03 17:59:27 +0900

    Use QueryDslPredicateExecutor to query articles

--- src/main/java/network/pluto/bibliotheca/repositories/ArticleRepository.java
+++ src/main/java/network/pluto/bibliotheca/repositories/ArticleRepository.java
@@ -5,10 +5,11 @@ import network.pluto.bibliotheca.models.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 
 import java.util.List;
 
-public interface ArticleRepository extends JpaRepository<Article, Long>, ArticleRepositoryCustom {
+public interface ArticleRepository extends JpaRepository<Article, Long>, QueryDslPredicateExecutor<Article>, ArticleRepositoryCustom {
     Page<Article> findByCreatedBy(Member createdBy, Pageable pageable);
     Page<Article> findByArticleIdIn(List<Long> articleIds, Pageable pageable);
     long countByCreatedBy(Member createdBy);

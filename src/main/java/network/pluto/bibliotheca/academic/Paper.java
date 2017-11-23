6928046b2b23f874d2ee851882c36cb2df834b84
tedyeee <tedyeee@gmail.com>
2017-11-23 17:15:32 +0900

    Added single paper query api

--- src/main/java/network/pluto/bibliotheca/academic/Paper.java
+++ src/main/java/network/pluto/bibliotheca/academic/Paper.java
@@ -2,11 +2,13 @@ package network.pluto.bibliotheca.academic;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
 
 @Getter
 @Setter
+@Table(name = "mag_paper")
 @Entity
 public class Paper {
 
@@ -14,4 +16,60 @@ public class Paper {
     @SequenceGenerator(name = "paperSequence", sequenceName = "paper_sequence", allocationSize = 1)
     @Id
     private long id;
+
+    @Column(nullable = false)
+    private String magId;
+
+    @Type(type = "text")
+    @Lob
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private int year;
+
+    @Column
+    private Integer nCitation;
+
+    @Type(type = "text")
+    @Lob
+    @Column(name = "abstract")
+    private String paperAbstract;
+
+    @Type(type = "text")
+    @Lob
+    @Column
+    private String urls;
+
+    @Column
+    private String lang;
+
+    @Column
+    private String doi;
+
+    @Column
+    private String docType;
+
+    @Type(type = "text")
+    @Lob
+    @Column
+    private String journalName;
+
+    @Column
+    private Integer journalId;
+
+    @Column
+    private String publisher;
+
+    @Column
+    private Integer volume;
+
+    @Column
+    private Integer issue;
+
+    @Column
+    private String pageStart;
+
+    @Column
+    private String pageEnd;
 }
---
+++ src/main/java/network/pluto/bibliotheca/academic/PaperRepository.java
@@ -0,0 +1,6 @@
+package network.pluto.bibliotheca.academic;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaperRepository extends JpaRepository<Paper, Long> {
+}

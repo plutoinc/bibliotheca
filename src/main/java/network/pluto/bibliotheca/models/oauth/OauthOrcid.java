2a6eb5780f9ed06e841ca087f1a8e693570e345e
tedyeee <tedyeee@gmail.com>
2017-12-12 15:39:09 +0900

    Refactor Orcid entity
    - rename Orcid to OauthOrcid
    - delete OauthOrcidFacade

---
+++ src/main/java/network/pluto/bibliotheca/models/oauth/OauthOrcid.java
@@ -0,0 +1,40 @@
+package network.pluto.bibliotheca.models.oauth;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import network.pluto.bibliotheca.models.BaseEntity;
+import network.pluto.bibliotheca.models.Member;
+
+import javax.persistence.*;
+import java.util.Map;
+import java.util.UUID;
+
+@ToString(exclude = { "member" })
+@Getter
+@Setter
+@Entity
+public class OauthOrcid extends BaseEntity {
+
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "oauthSequence")
+    @SequenceGenerator(name = "oauthSequence", sequenceName = "oauth_sequence", allocationSize = 1)
+    @Id
+    private long id;
+
+    @Column(nullable = false)
+    private String uuid = UUID.randomUUID().toString();
+
+    @Column(nullable = false, unique = true)
+    private String orcid;
+
+    @Column(nullable = false)
+    private String accessToken;
+
+    @Transient
+    private Map<String, Object> userData;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MEMBER_ID", unique = true)
+    private Member member;
+}

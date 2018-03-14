d726fb8b405da551109d1a08d6c1ff962e90afb5
tedyeee <tedyeee@gmail.com>
2018-03-14 10:56:28 +0900

    Removed unused entities
    - removed Author
    - removed Fos
    - removed Journal
    - removed PaperReference

--- src/main/java/network/pluto/bibliotheca/enums/AuthorType.java
+++
@@ -1,7 +0,0 @@
-package network.pluto.bibliotheca.enums;
-
-public enum AuthorType {
-    LEAD_AUTHOR,
-    CORRESPONDING_AUTHOR,
-    CO_AUTHOR
-}
--- src/main/java/network/pluto/bibliotheca/repositories/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package network.pluto.bibliotheca.repositories;
-
-import network.pluto.bibliotheca.models.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}

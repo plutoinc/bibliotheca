1fdbdb3976e1fd82ecbd22434fed41e657be34af
tedyeee <tedyeee@gmail.com>
2018-03-23 11:17:32 +0900

    Serve aggregation API for Journal/FOS

---
+++ src/main/java/network/pluto/bibliotheca/repositories/FieldsOfStudyRepository.java
@@ -0,0 +1,10 @@
+package network.pluto.bibliotheca.repositories;
+
+import network.pluto.bibliotheca.models.mag.FieldsOfStudy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FieldsOfStudyRepository extends JpaRepository<FieldsOfStudy, Long> {
+    List<FieldsOfStudy> findByIdIn(List<Long> fosIds);
+}

8523fdc6ff731fc52be9c89f87c2a0d2dfeacfc2
tedyeee <tedyeee@gmail.com>
2018-04-04 16:01:01 +0900

    Do not use count query for ref/citation pagination
    - count query is very slow for ref/citation counting

--- src/main/java/network/pluto/bibliotheca/repositories/mag/RelPaperReferenceRepository.java
+++ src/main/java/network/pluto/bibliotheca/repositories/mag/RelPaperReferenceRepository.java
@@ -1,11 +1,12 @@
 package network.pluto.bibliotheca.repositories.mag;
 
 import network.pluto.bibliotheca.models.mag.RelPaperReference;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RelPaperReferenceRepository extends JpaRepository<RelPaperReference, RelPaperReference.RelPaperReferenceId> {
-    Page<RelPaperReference> findByPaperId(long paperId, Pageable pageable);
-    Page<RelPaperReference> findByPaperReferenceId(long referenceId, Pageable pageable);
+    List<RelPaperReference> findByPaperId(long paperId, Pageable pageable);
+    List<RelPaperReference> findByPaperReferenceId(long referenceId, Pageable pageable);
 }

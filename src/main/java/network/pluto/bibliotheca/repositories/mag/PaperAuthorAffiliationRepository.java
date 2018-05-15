49328be06f2cd4fc453ad87efc36c59818881dfe
tedyeee <tedyeee@gmail.com>
2018-05-15 15:18:47 +0900

    Support author page API

--- src/main/java/network/pluto/bibliotheca/repositories/mag/PaperAuthorAffiliationRepository.java
+++ src/main/java/network/pluto/bibliotheca/repositories/mag/PaperAuthorAffiliationRepository.java
@@ -2,6 +2,7 @@ package network.pluto.bibliotheca.repositories.mag;
 
 import network.pluto.bibliotheca.models.mag.Paper;
 import network.pluto.bibliotheca.models.mag.PaperAuthorAffiliation;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,5 +14,14 @@ public interface PaperAuthorAffiliationRepository extends JpaRepository<PaperAut
 
     @Query("select r.paper from PaperAuthorAffiliation r join r.paper where r.id.authorId = :authorId and not r.id.paperId = :paperId order by r.authorSequenceNumber asc, r.paper.citationCount desc")
     List<Paper> getAuthorMainPapers(@Param("paperId") long paperId, @Param("authorId") long authorId, Pageable pageable);
+
+    @Query("select r.paper from PaperAuthorAffiliation r join r.paper where r.id.authorId = :authorId order by r.paper.citationCount desc")
+    Page<Paper> getAuthorPapersMostCitations(@Param("authorId") long authorId, Pageable pageable);
+
+    @Query("select r.paper from PaperAuthorAffiliation r join r.paper where r.id.authorId = :authorId order by r.paper.year desc")
+    Page<Paper> getAuthorPapersNewest(@Param("authorId") long authorId, Pageable pageable);
+
+    @Query("select r.paper from PaperAuthorAffiliation r join r.paper where r.id.authorId = :authorId order by r.paper.year asc")
+    Page<Paper> getAuthorPapersOldest(@Param("authorId") long authorId, Pageable pageable);
 
 }

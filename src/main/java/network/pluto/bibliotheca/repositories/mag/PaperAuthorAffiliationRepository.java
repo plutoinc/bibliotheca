b5708139bca427556c1b7ecabbaefe92e1884bb2
tedyeee <tedyeee@gmail.com>
2018-06-28 18:09:01 +0900

    Modify co-author sql

--- src/main/java/network/pluto/bibliotheca/repositories/mag/PaperAuthorAffiliationRepository.java
+++ src/main/java/network/pluto/bibliotheca/repositories/mag/PaperAuthorAffiliationRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.math.BigInteger;
 import java.util.List;
 
 public interface PaperAuthorAffiliationRepository extends JpaRepository<PaperAuthorAffiliation, PaperAuthorAffiliation.PaperAuthorAffiliationId> {
@@ -23,8 +22,5 @@ public interface PaperAuthorAffiliationRepository extends JpaRepository<PaperAut
 
     @Query("select r.paper from PaperAuthorAffiliation r join r.paper where r.id.authorId = :authorId order by r.paper.year asc")
     List<Paper> getAuthorPapersOldest(@Param("authorId") long authorId, Pageable pageable);
-
-    @Query(value = "select author_id from mcsa.rel_paper_author_affiliation where paper_id in (select paper_id from mcsa.rel_paper_author_affiliation where author_id = :authorId) and not author_id = :authorId group by author_id order by count(author_id) desc limit 5", nativeQuery = true)
-    List<BigInteger> findCoAuthors(@Param("authorId") long authorId);
 
 }

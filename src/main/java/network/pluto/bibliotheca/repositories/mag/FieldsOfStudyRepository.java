ec40d6547a688351533b0a0d9d5efb596467c964
tedyeee <tedyeee@gmail.com>
2018-07-03 22:24:04 +0900

    Support journal page APIs (#59)
    * Move package
    
    * Add commons dependency

--- src/main/java/network/pluto/bibliotheca/repositories/mag/FieldsOfStudyRepository.java
+++ src/main/java/network/pluto/bibliotheca/repositories/mag/FieldsOfStudyRepository.java
@@ -1,4 +1,4 @@
-package network.pluto.bibliotheca.repositories;
+package network.pluto.bibliotheca.repositories.mag;
 
 import network.pluto.bibliotheca.models.mag.FieldsOfStudy;
 import org.springframework.data.jpa.repository.JpaRepository;

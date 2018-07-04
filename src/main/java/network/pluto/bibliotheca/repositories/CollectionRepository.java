88ad35756a1d2a3e5c9924de107c8c4d5efb3c5f
tedyeee <tedyeee@gmail.com>
2018-07-04 18:08:59 +0900

    Limit number of collection for each member (#60)

--- src/main/java/network/pluto/bibliotheca/repositories/CollectionRepository.java
+++ src/main/java/network/pluto/bibliotheca/repositories/CollectionRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CollectionRepository extends JpaRepository<Collection, Long> {
     Page<Collection> findByCreatedByOrderByCreatedAtDesc(Member creator, Pageable pageable);
+    long countByCreatedBy(Member creator);
 }
